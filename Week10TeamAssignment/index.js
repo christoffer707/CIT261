6bc7f6d8b43d53dbea4ac555da4541cccd2da452
christoffer707 <christoffer707@gmail.com>
2019-06-28 20:29:40 -0700

    Added team assignment

---
+++ Week10TeamAssignment/index.js
@@ -0,0 +1,31 @@
+/*jslint es6: true  */
+
+import {getLocation} from 'utilities';
+import {getJSON} from 'utilities';
+
+
+var baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
+
+getLocation()
+.then( (result) => {
+   const lat = result.coords.latitude;
+   const long = result.coords.longitude;
+
+   baseUrl += '&latitude=' + lat +
+                  '&longitude=' + long +
+                     '&maxradiuskm=100';
+
+   console.log(baseUrl);
+
+   getJSON(baseUrl)
+      .then( (result) => {
+         console.log(result);
+         result.features.forEach((features) => {
+            var li = document.createElement('li');
+            li.textContent = features.properties.place;
+         });
+         ul.appendChild(li);
+      }) 
+});
+
+
